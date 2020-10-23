76c8878fa4b288847ac87679998ae10cf025c2f7
Refyled <romaindelyfer@gmail.com>
2020-10-23 13:09:50 +0200

    gitIgnore

--- test.js
+++ test.js
@@ -1,47 +1,13 @@
+const io = require('socket.io-client');
+const socket = io("http://localhost:3000", {reconnectionDelayMax: 10000});
+const testSt	= {initialPos:"default",initialWeight:10,gridSize:10,NBVit:1};
+socket.emit('playerIdentification','default');
+socket.on('ID',(res)=>{console.log(res)});
+socket.emit('newGame',testSt);
 
-const compute = require('./index.js');
+socket.emit('reqLiveGamesList');
+socket.on('gameList', (data)=>console.log(data))
 
-//-------------------------------------------------------
+socket.emit('playOnGame',0);
 
-//const displayGridWithPlayers()
-
-
-//-------------------------------------------------------
-
-console.log("testing");
-
-testArray = [
-{name:"cell1",
-x:3,
-y:4,
-move:["r","u"],
-weight:3,
-state:0,
-player:"J"},
-{name:"cell2",
-x:5,
-y:4,
-move:["l"],
-weight:4,
-state:0,
-player:"P"},
-{name:"cell3",
-x:8,
-y:8,
-move:["r"],
-weight:6,
-state:0,
-player:"J"},
-{name:"cell4",
-x:8,
-y:6,
-move:["r"],
-weight:5,
-state:0,
-player:"R"}
-];
-console.log("_____________initial situation :____________");
-console.log(testArray);
-console.log("-----------------result :---------------");
-console.log(compute(testArray,10,1));
-
+socket.emit('startGame',0);	
